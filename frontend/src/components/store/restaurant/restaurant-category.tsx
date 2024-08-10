70aeaf839c547eb1a1c4d7b43d2cb05c579eb33a
Jess Lin <jess@render.com>
2024-08-10 16:14:02 -0400

    Change main heading on home page

--- frontend/src/components/store/restaurant/restaurant-category.tsx
+++ frontend/src/components/store/restaurant/restaurant-category.tsx
@@ -12,7 +12,7 @@ export default function RestaurantCategory({
 }) {
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-2xl font-medium">{categoryName}Popular near you</h2>
+      <h2 className="text-2xl font-medium">{categoryName}The best food in your hood</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-3">
         {restaurants.map((restaurant) => (
           <Link
